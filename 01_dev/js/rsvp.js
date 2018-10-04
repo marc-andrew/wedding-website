4e5a890f98a093986cd4f3bc2b5b503bee116bf9
Marc Andrew <hello@marcandrew.net>
2018-10-04 08:13:30 +0100

    updates

--- 01_dev/js/rsvp.js
+++ 01_dev/js/rsvp.js
@@ -14,7 +14,7 @@
         let rsvpDb = db.collection('rsvp');
 
         usersDb.where("e", "==", 'email@email.com').get().then(function (querySnapshot) {
-            if(querySnapshot.empty) {
+            if (querySnapshot.empty) {
                 console.log('can\'t find');
             } else {
                 querySnapshot.forEach(function (doc) {
@@ -44,12 +44,102 @@
         let rsvpConfirmed = db.collection('rsvpConfirmed');
         let batch = db.batch();
 
-        batch.set(rsvp.doc("valookaran"), {
+        batch.set(rsvp.doc("puntigam"), {
             a: false,
             c: false,
             dC: null,
             n: null,
-            mG: 0,
+            mg: 3,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("makela"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 1,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("phutusseril"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 1,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("waight"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 1,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("cartocci"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 1,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("pfabigan"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 1,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("hausberger"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 1,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("sheth"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 0,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("schneider"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 1,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("makinwa"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 0,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("sui Qi"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 0,
             aG: 0,
             gN: null
         });
@@ -58,7 +148,115 @@
             c: false,
             dC: null,
             n: null,
-            mG: 1,
+            mg: 1,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("paille de riviere"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 1,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("mazuranic"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 0,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("borensztein"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 1,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("ringhofer"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 0,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("lueng"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 0,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("walters"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 0,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("roberts"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 1,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("eichert"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 0,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("kernbauer"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 1,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("brenner"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 0,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("fitch"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 1,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("kaiser"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 1,
             aG: 0,
             gN: null
         });
@@ -67,17 +265,45 @@
             c: false,
             dC: null,
             n: null,
-            mG: 0,
+            mg: 0,
             aG: 0,
             gN: null
         });
-        // batch.update(rsvp.doc("kaiser"),{
-        //     a: true,
-        //     c: true
-        // });
-        batch.set(rsvpConfirmed.doc('kaiser'), {
-            a: true,
-            aG: 1,
+        batch.set(rsvp.doc("valokaran"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 1,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("ehimare"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 0,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("allison"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 0,
+            aG: 0,
+            gN: null
+        });
+        batch.set(rsvp.doc("jewkes"), {
+            a: false,
+            c: false,
+            dC: null,
+            n: null,
+            mg: 1,
+            aG: 0,
+            gN: null
         });
 
         // Batch commit 
